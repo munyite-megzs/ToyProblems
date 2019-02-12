cd0780b65f6539432f75685719e40a7fcfdf8d2a
munyite-megzs <margaret.sitati1@gmail.com>
2019-02-12 11:14:39 +0300

    A Very Big sum Toy Problem

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Averybigsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long results =0L;
+
+            // get the array
+            // get the array element
+            //use the sum method of an array to sum up 
+
+            long[] sumNo = { 1000000001,1000000002,1000000003,1000000004,1000000005 };
+
+            results = sumNo.Sum();
+            //for (long i = 0; i < sumNo.Length; i++)
+            //{
+            //    results += sumNo[i];
+            //}
+            Console.WriteLine(results);
+            Console.ReadLine();
+        }
+    }
+}
